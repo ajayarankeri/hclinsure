02db7d6fe9c1c53821f330157f97642d260b0615
rksiet2 <rksiet2@gmail.com>
2019-08-01 19:26:02 +0530

    Analysis report commit

--- src/main/java/com/hcl/hclinsure/service/CustomerPolicyService.java
+++ src/main/java/com/hcl/hclinsure/service/CustomerPolicyService.java
@@ -2,12 +2,10 @@ package com.hcl.hclinsure.service;
 
 import java.io.ByteArrayInputStream;
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.stereotype.Service;
 
 import com.hcl.hclinsure.dto.CustomerPolicyDto;
-import com.hcl.hclinsure.entity.CustomerPolicy;
 
 @Service
 public interface CustomerPolicyService {
