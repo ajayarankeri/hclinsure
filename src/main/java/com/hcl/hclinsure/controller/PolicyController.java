2a1d3c0490f77d43790e66560dbcce45ccdc9977
ajayarankeri <ajayarankeri@gmail.com>
2019-08-01 14:18:41 +0530

    changes commit

--- src/main/java/com/hcl/hclinsure/controller/PolicyController.java
+++ src/main/java/com/hcl/hclinsure/controller/PolicyController.java
@@ -3,6 +3,7 @@ package com.hcl.hclinsure.controller;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -13,6 +14,7 @@ import com.hcl.hclinsure.service.PolicyService;
 
 @RestController
 @RequestMapping("/policies")
+@CrossOrigin(origins = "*", allowedHeaders = "*")
 public class PolicyController {
 	
 	@Autowired
